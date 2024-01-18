696aaa9b2e11a3c07d3a6431432de122b9820980
S0mbr4xc <s3._xc@hotmail.com>
2024-01-18 08:34:00 -0500

    Project

--- prueba63Cordova/src/app/app.module.ts
+++ prueba63Cordova/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    IngresoComponent
   ],
   imports: [
     BrowserModule,
